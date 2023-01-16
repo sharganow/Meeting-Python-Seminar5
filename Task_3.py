47dc44dd6c9ac2774f86713e5b0930e4c996591b
sharganow <sharganow@gmail.com>
2023-01-16 23:29:21 +0500

    Покахал дочери

--- Task_3.py
+++ Task_3.py
@@ -2,7 +2,7 @@
 # НЕОБЯЗАТЕЛЬНО Добавить игру против бота с интеллектом
 
 
-def get_ures_name(plr: int) -> str:
+def get_user_name(plr: int) -> str:
     name = input(f'Введите имя игрока №{plr}: ')
     return name
 
@@ -45,15 +45,64 @@ def showBoard(board: list):
             print(f' ---|---|--- ')
 
 
+def search_for_a_winner(board: list) -> str:
+    for i in range(3):
+        string = board[i][0]
+        column = board[0][i]
+        for j in range(3):
+            if string != board[i][j]:
+                string = 'No'
+            if column != board[j][i]:
+                column = 'No'
+        if string != 'No':
+            return string
+        if column != 'No':
+            return column
+    center = board[1][1]
+    for i in range(3):
+        if center != board[2 - i][i]:
+            center = 'No'
+    if center != 'No':
+        return center
+    center = board[1][1]
+    for i in range(3):
+        if center != board[i][i]:
+            center = 'No'
+    return center
+
+
+def enter_sign(board: list, entr: int, sign: str):
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == entr:
+                board[i][j] = sign
+                break
+        else:
+            continue
+        break
+    else:
+        print('Выбранная ячейка занята')
+
+def
+
+
+
 gameBoard = [[(string * 3 + column) for column in range(3)] for string in range(3)]
-players = [get_ures_name(i) for i in range(1, 3)]
+# players = [get_user_name(i) for i in range(1, 3)]
+#
+# whoMakeChoice = dict()
+# choice_of_decision_algorithm(players, whoMakeChoice)
+# userSign = dict()
+# choice_sign_to_play(players, userSign)
+#
+# print(*sorted(whoMakeChoice.items()))
+showBoard(gameBoard)
+print(search_for_a_winner(gameBoard))
 
+gameBoard[2][0] = gameBoard[1][1] = gameBoard[0][2] = 'Y'
+showBoard(gameBoard)
+enter_sign(gameBoard, int(input('Выберите поле: ')), 'X')
+showBoard(gameBoard)
 
+print(search_for_a_winner(gameBoard))
 
-whoMakeChoice = dict()
-choice_of_decision_algorithm(players, whoMakeChoice)
-userSign = dict()
-choice_sign_to_play(players, userSign)
-
-print(*sorted(whoMakeChoice.items()))
-# showBoard(gameBoard)
